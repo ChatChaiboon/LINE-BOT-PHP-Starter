c989bcd8bb7410f8fe4a3b3cc169f37e2a586bd4
ChatChaiboon <chatchaiboon01@gmail.com>
2017-06-21 21:03:48 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'kqQ3jhXCO0erfnyGfDAbwrVLTCVO4uzmDlj95zJCBrMmSagd7VU887nbNa4+uwaq/DK96xySrqvcezviH9MQKusPTPAO8YUZvp7igFIceC7oPtOxS9A3m2nO2+8gJKU9geExJ+UIqL7n4s2Jt5FCWgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
